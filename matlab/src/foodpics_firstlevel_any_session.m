c1b1abeaafc226ec551419f8853347d06ae44bf1
Maureen McHugo <maureen.mchugo@cuanschutz.edu>
2024-11-04 13:58:59 -0700

    Updated path to main data folder on bk

--- matlab/src/foodpics_firstlevel_any_session.m
+++ matlab/src/foodpics_firstlevel_any_session.m
@@ -14,10 +14,10 @@ end
 
 %% Study specific variables to specify data folders and SPM settings
 task_id='foodpics';
-study_dir='/home/maureen/adak';								        % main study directory
+study_dir='/home/data/images/adak';								    % main study directory
 bids_dir=fullfile(study_dir,'data','bids_data');					% bids data directory
 preproc_dir=fullfile(bids_dir, 'derivatives','fmriprep_3mm');		% directory with preprocessed data
-output_dir=fullfile(bids_dir,'derivatives',['adak_' task_id '_TEST']);	    % analysis output directory for fmriprep 3mm data
+output_dir=fullfile(bids_dir,'derivatives',['adak_' task_id '_spm']);	    % analysis output directory for fmriprep 3mm data
 
 n_vols='172';                           % each run should contain 172 volumes
 tr='2';                                 % repetition time = 2s
