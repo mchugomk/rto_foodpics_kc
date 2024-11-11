d2e04c2b528f7fbf67b28de779158eb77eba4853
Maureen McHugo <maureen.mchugo@cuanschutz.edu>
2024-11-11 08:36:49 -0700

    Updated paths to use rto folders instead of adak

--- matlab/src/foodpics_firstlevel_any_session.m
+++ matlab/src/foodpics_firstlevel_any_session.m
@@ -8,11 +8,12 @@ function foodpics_firstlevel_any_session(subject_id, session_id, run_list)
 %   to process only run 1: [1]
 %   to process only run 2: [2]
 %
-% This script runs a pipeline to analyze the ADAK foodpics data
+% This script runs a pipeline to analyze the RTO foodpics data
 % based on parameters determined by KC. The pipeline includes:
 % Changes from adak_foodpics_spm:
 % - Saves residuals if needed for AFNI analysis
-% - Saves output in folder 'adak_foodpics_kc'
+% Changes from adak_foodpics_kc:
+% - Saves output in folder 'rto_foodpics_kc'
 
 if nargin ~= 3 
     error('Must specify subject_id, session_id and run_list');
@@ -20,11 +21,11 @@ end
 
 %% Study specific variables to specify data folders and SPM settings
 task_id='foodpics';
-study_dir='/home/data/images/adak';								                % main study directory
-onset_dir=fullfile(study_dir, 'code', ['adak_' task_id '_kc'],'matlab','src');  % directory with SPM onsets - same for all subjects in this study
+study_dir='/home/data/images/rto';								                % main study directory
+onset_dir=fullfile(study_dir, 'code', ['rto_' task_id '_kc'],'matlab','src');   % directory with SPM onsets - same for all subjects in this study
 bids_dir=fullfile(study_dir,'data','bids_data');					            % bids data directory
 preproc_dir=fullfile(bids_dir, 'derivatives','fmriprep_3mm');		            % directory with preprocessed data
-output_dir=fullfile(bids_dir,'derivatives',['adak_' task_id '_kc']);            % analysis output directory for fmriprep 3mm data
+output_dir=fullfile(bids_dir,'derivatives',['rto_' task_id '_kc']);             % analysis output directory for fmriprep 3mm data
 
 n_vols='172';                           % each run should contain 172 volumes
 tr='2';                                 % repetition time = 2s
